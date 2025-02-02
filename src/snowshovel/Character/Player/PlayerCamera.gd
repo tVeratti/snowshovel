0f36ea7a2271ea5aca892a7e08a7628d630a8b3d
Tatiana Veratti <TatianaVeratti@gmail.com>
2025-02-02 01:07:20 -0600

    add snow and anti-snow mask

--- src/snowshovel/Character/Player/PlayerCamera.gd
+++ src/snowshovel/Character/Player/PlayerCamera.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 
 
-const MOVE_SPEED:float = 6.0
+const MOVE_SPEED:float = 10.0
 
 
 @export var target:Node3D
@@ -13,6 +13,6 @@ const MOVE_SPEED:float = 6.0
 func _physics_process(delta):
 	if is_instance_valid(target):
 		var target_position:Vector3 = target.global_position + initial_offset
-		global_position = lerp(global_position, target_position, MOVE_SPEED * delta)
+		global_position = target_position # lerp(global_position, target_position, MOVE_SPEED * delta)
 		
 		look_at(target.global_position)
