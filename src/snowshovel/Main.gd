a7818ded441db84b7ec0f19d5b77a3bb875d8284
Tatiana Veratti <TatianaVeratti@gmail.com>
2025-02-06 11:47:08 -0600

    add play game transition

--- src/snowshovel/Main.gd
+++ src/snowshovel/Main.gd
@@ -6,11 +6,17 @@ const PLAY_ANIMATION_DURATION:float = 2.0
 @onready var player:Player = $Player
 @onready var menu:Control = %MenuInterface
 
+@onready var play_button:Button = menu.find_child("Play")
+@onready var quit_button:Button = menu.find_child("Quit")
+
 
 func _ready():
 	# Zoom the initial camera view out, so when the game starts, it can zoom IN
-	player.camera.offset_override = player.camera.initial_offset * 2.0
-	player.camera.fov = 80
+	player.camera.offset_override = player.camera.initial_offset * 1.2
+	#player.camera.fov = 80
+	
+	play_button.pressed.connect(_on_play_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 	
 	pause_player(true)
 
@@ -21,13 +27,20 @@ func pause_player(is_paused) -> void:
 	player.set_process_input(!is_paused)
 
 
-func _on_play_pressed():
+func _on_play_pressed() -> void:
 	var camera:PlayerCamera = player.camera
 	
+	play_button.hide()
+	quit_button.hide()
+	
 	var tween: = get_tree().create_tween()
-	tween.tween_property(menu, "modulate", Color(1, 1, 1, 0), PLAY_ANIMATION_DURATION) 
+	tween.tween_property(menu, "position", Vector2(-999, 0), PLAY_ANIMATION_DURATION) 
 	tween.parallel().tween_property(camera, "offset_override", Vector3.ZERO, PLAY_ANIMATION_DURATION)
-	tween.parallel().tween_property(camera, "fov", 75, PLAY_ANIMATION_DURATION)
+	#tween.parallel().tween_property(camera, "fov", 75, PLAY_ANIMATION_DURATION)
 	tween.tween_callback(func():
 		pause_player(false)
 		menu.queue_free())
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
